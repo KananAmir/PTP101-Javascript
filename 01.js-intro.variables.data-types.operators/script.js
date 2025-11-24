b8790b86fb9a243e856bf1881efc2ea9eb8c6235
KananAmir <kanan.amirov@code.edu.az>
2025-11-24 13:09:43 +0400

    js intro, variables

---
+++ 01.js-intro.variables.data-types.operators/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+// window.alert("salamlar")
+// document.writeln(
+//     `<h1>lorem ipsum dolor</h1>`
+// )
+
+// debugging
+
+// javascript single thread
---
+++ 01.js-intro.variables.data-types.operators/variables.js
@@ -0,0 +1,88 @@
+// console.log("variables");
+
+// let, const, var
+
+
+
+let userName = "elmir_aslan";
+userName = "akbar"
+let userAge = 19;
+
+
+console.log(userName);
+console.log(userAge);
+
+// camelCase
+// PascalCase
+// snake_case
+// kabab-case
+
+
+
+let isOnline;
+
+isOnline = false;
+
+// let const var
+
+
+// block scope variables: let and const
+//function scope variable: var
+// const COLOR_RED = '#ff0000'
+// COLOR_RED = '#ffff00'
+// console.log(COLOR_RED);
+
+let x = 15;
+x = 43
+
+console.log(x);
+
+
+const API_URL = 'https://northwind.vercel.app/api/categories'
+
+{
+    let user = 'farman'
+    console.log(user);
+}
+
+
+
+// let city = "baku"
+
+// let city = "london"
+
+// console.log("city:", city);
+
+var score = 85
+
+var score = 15
+
+console.log(score);
+
+
+
+{
+    var y = 44
+}
+
+console.log(y);
+
+
+function sayHi(){
+    var user = "huseynagha"
+    console.log('Hello, ', user);
+}
+
+sayHi()
+
+// hoisting
+// console.log(groupName);
+
+// let groupName; // declare
+// groupName = "PTP101"
+
+
+
+console.log(country);
+var country = "Azerbaijan"
+
