65f28c5aa787d621564d88a511e8e29a51ac11e8
KananAmir <kanan.amirov@code.edu.az>
2025-12-12 14:43:03 +0400

    js module

---
+++ 11.js-module/assets/js/main.js
@@ -0,0 +1,27 @@
+import { groupName as qrupAdi, sum } from './data.js'
+import allProducts from './data.js';
+
+let groupName = 'PF103'
+console.log(qrupAdi);
+
+console.log(sum(1, 2));
+
+const products = [
+    { name: "one Product 1", price: 20, category: "electronics" },
+    { name: "two Product 2", price: 38, category: "Clothes" },
+    { name: "three Product 3", price: 43, category: "Electronics" },
+    { name: "four Product 4", price: 60, category: "Clothes" },
+    { name: "five Product 5", price: 35, category: "Clothes" },
+    { name: "six Product 6", price: 70, category: "Electronics" },
+    { name: "seven Product 7", price: 10, category: "Clothes" },
+    { name: "eight Product 8", price: 85, category: "electronics" },
+];
+
+
+
+// named import/export
+// default import/export
+
+
+console.log(products);
+console.log(allProducts);
