c3deaecf90ff3706db422013f0d8c9ff7be866a4
KananAmir <kanan.amirov@code.edu.az>
2025-12-17 14:02:48 +0400

    git add .

--- 13.js-dom/assets/js/app.js
+++ 13.js-dom/assets/js/app.js
@@ -24,12 +24,12 @@ const wrapper = document.querySelector('.wrapper')
 
 parag.textContent = 'Lorem ipsum dolor!'
 
-const btn = document.createElement('button');
-btn.textContent = 'click'
+const clickBtn = document.createElement('button');
+clickBtn.textContent = 'click'
 
 // wrapper.append(parag, btn)
 wrapper.appendChild(parag)
-wrapper.appendChild(btn)
+wrapper.appendChild(clickBtn)
 // wrapper.prepend(parag)
 
 
@@ -56,8 +56,12 @@ const addBtn = document.querySelector('.add-btn')
 const todoList = document.querySelector('.todo-list')
 
 //js events - click
+// const allLiElems = document.querySelectorAll('li') // node list
+const allLiElems = document.getElementsByTagName('li') // html collection
 
-addBtn.addEventListener('click', () => {
+// console.log(allLiElems);
+
+addBtn.addEventListener('click', function () {
     const value = todoInput.value
     if (!value) {
         alert('input bos ola bilmez!!!')
@@ -65,7 +69,100 @@ addBtn.addEventListener('click', () => {
     }
     const li = document.createElement('li')
     li.textContent = value
+
+
+
+    li.addEventListener('click', (event)=>{
+        // console.log(event.target); 
+        event.target.remove()
+    })
+
     todoList.appendChild(li)
 
     todoInput.value = ''
+
+
+    console.log(allLiElems);
+
+})
+
+
+const heading = document.createElement('h2')
+heading.textContent = 'PTP101'
+// todoList.after(heading)
+// todoList.before(heading)
+
+
+const clearBtn = document.querySelector('.clear-btn')
+
+clearBtn.addEventListener('click', ()=>{
+    todoList.innerHTML = ''
+})
+
+
+
+const link = document.createElement('a')
+
+link.textContent = 'github.com'
+link.setAttribute('href', 'https://github.com/kananamir')
+link.setAttribute('target', '_blank')
+
+// document.querySelector('#banner').insertAdjacentElement('beforeend', link)
+document.querySelector('#banner').after(link)
+
+console.log('--------------------');
+
+const photo = document.createElement('img')
+
+photo.setAttribute('src', 'https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg')
+photo.setAttribute('alt', 'javascript logo')
+photo.setAttribute('width', '100')
+document.body.appendChild(photo)
+
+console.log(todoInput.getAttribute('name'));
+
+
+// todoInput.removeAttribute('placeholder')
+
+console.log(todoInput.hasAttribute('title')); // false
+console.log(todoInput.hasAttribute('id')); // true
+
+const header = document.querySelector('header')
+const btn = document.createElement('button')
+
+btn.textContent = 'click'
+
+btn.style.color = 'teal'
+btn.style.backgroundColor = 'transparent'
+btn.style.border = '2px solid teal'
+btn.style.cursor = 'pointer'
+btn.style.padding = '0.5rem 1rem'
+
+
+btn.addEventListener('click', ()=>{
+    note.style.color = 'red'
+})
+header.appendChild(btn)
+
+
+
+const firstNum = document.querySelector('.first')
+const secondNum = document.querySelector('.second')
+const sumBtn = document.querySelector('.sum')
+const multiplyBtn = document.querySelector('.multiply')
+const divideBtn = document.querySelector('.divide')
+const subtructBtn = document.querySelector('.subtruct')
+const result = document.querySelector('.result')
+
+sumBtn.addEventListener('click', ()=>{
+    result.textContent = Number(firstNum.value) + Number(secondNum.value )
+})
+multiplyBtn.addEventListener('click', ()=>{
+    result.textContent = Number(firstNum.value) * Number(secondNum.value )
+})
+divideBtn.addEventListener('click', ()=>{
+    result.textContent = Number(firstNum.value) / Number(secondNum.value )
+})
+subtructBtn.addEventListener('click', ()=>{
+    result.textContent = Number(firstNum.value) - Number(secondNum.value )
 })
